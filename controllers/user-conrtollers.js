f126289204a65e8fdc7ba02822d37f9be2296371
Brad Cassity <bradley.cassity@gmail.com>
2020-12-06 18:36:21 -0700

    Added ability to add/remove friends

--- controllers/user-conrtollers.js
+++ controllers/user-conrtollers.js
@@ -69,7 +69,19 @@ const userController = {
 
     // DELETE - Remove friend
     removeFriend({ params, body }, res) {
-
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends:  params.friendId } },
+            { new: true }
+        )
+            .then(userData => {
+                if(!userData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(userData);
+            })
+            .catch(err => res.status(400).json(err));
     }
 };
 
